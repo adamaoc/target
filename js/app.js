e4d816be02ee49ad89c590c19a8c361eb360f26c
adamaoc <adamaoc@gmail.com>
2013-07-30 12:37:13 -0500

    making everything break like crazy

--- js/app.js
+++ js/app.js
@@ -1,85 +1,61 @@
 var App = Ember.Application.create({
     LOG_TRANSITIONS: true
-    });
+});
 
 
 App.Router.map(function() {
-	this.resource('dashboard');
 	this.resource('users');
-});
-
-App.UsersRoute = Ember.Route.extend({
-	model: function() {
-		return App.User.find();
-	}
-});
-App.IndexRoute = Ember.Route.extend({
-	model: function() {
-		return App.User.find(1);
-	}
-});
-
-App.DashboardRoute = Ember.Route.extend({
-	model: function() {
-		return App.User.find(1);
-	}
-});
-
-App.ClickableView = Ember.View.extend({
-  click: function(evt) {
-  	var appFooter = $('.app-footer');
-  	var appNav = $('.app-nav');
-  	if (appFooter.hasClass('footer-open')) {
-  		appFooter.removeClass('footer-open');
-  		appNav.hide();
-  	} else {
-   		appFooter.addClass('footer-open');
-   		appNav.show();
-   	}
-  }
+	this.route('dashboard', { path: "/dashboard" });
 });
 
 App.Store = DS.Store.extend({
   revision: 12,
-  adapter: 'DS.FixtureAdapter'
+  adapter: DS.RESTAdapter.create({
+  	url: 'http://dev.ampnetmedia.com/api'
+  })
 });
 
 App.User = DS.Model.extend({
-	username: DS.attr('string'),
-	firstName: DS.attr('string'),
-	lastName: DS.attr('string'),
-	assignID: DS.attr('string'),
+	firstname: DS.attr('string'),
+	lastname: DS.attr('string'),
+	userID: DS.attr('string'),
 	bio: DS.attr('string'),
-	joinedDate: DS.attr('string'),
 	rank: DS.attr('string')
 });
 
-App.User.FIXTURES = [
-	{
-		"id": 1,
-		"username": "adamaoc",
-		"firstName": "Adam",
-		"lastName": "Moore",
-		"assignID": "1001",
-		"bio": "Working on the Enterprise team building out the new Ford Mobile Skin.",
-		"rank": "Assassin"
-	}, 
-	{
-		"id": 2,
-		"username": "mlusby",
-		"firstName": "Mark",
-		"lastName": "Lusby",
-		"assignID": "10202",
-		"bio": "Working on the Inventory Search team building out the new MVC Inventory pages!",
-		"rank": "Killer"
-	}, 
-	{
-		"id": 3,
-		"username": "jtowers",
-		"firstName": "Josh",
-		"lastName": "Towers",
-		"assignID": "10142",
-		"bio": "Work? What's that all about..",
-		"rank": "Rooky"
+App.IndexRoute = Ember.Route.extend({
+	model: function() {
+		$user = App.User.find();
+		console.log($user);
+		return App.User.find();
+	},
+	// events: {
+	// 	logIn: function() {
+	// 		// check credentials
+	// 		var $username = $('#username').val(),
+	// 			$password = $('#password').val();
+	// 			$user = this.modelFor('User');
+
+	// 		if ($username == 'adam' & $password == 'test') {
+	// 			this.transitionTo('smallDash');
+	// 			console.log($user);
+	// 		} else {
+	// 			alert('error');
+	// 		}
+	// 	}
+	// },
+});
+
+App.UsersRoute = Ember.Route.extend({
+	model: function() {
+		// return App.User.find();
+		return App.User.find();
 	}
-];
+});
+
+App.dashboard = Ember.Route.extend({
+	model: function(params) {
+		// return users[params.user_id];
+		return App.User.find(params.userID);
+	}
+});
